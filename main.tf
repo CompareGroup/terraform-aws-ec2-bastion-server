67caf0fcb3e7f45f5249496b475694aac06525b0
Ronald Konjer <ronaldkonjer@gmail.com>
2022-02-08 23:06:30 +0100

    bug-fix: do not create spot and ondemand

--- main.tf
+++ main.tf
@@ -118,7 +118,7 @@ resource "aws_spot_instance_request" "default" {
 resource "aws_instance" "default" {
   #bridgecrew:skip=BC_AWS_PUBLIC_12: Skipping `EC2 Should Not Have Public IPs` check. NAT instance requires public IP.
   #bridgecrew:skip=BC_AWS_GENERAL_31: Skipping `Ensure Instance Metadata Service Version 1 is not enabled` check until BridgeCrew support condition evaluation. See https://github.com/bridgecrewio/checkov/issues/793
-  count                       = module.this.enabled ? 1 : 0
+  count                       = local.use_ondemand ? 1 : 0
   ami                         = data.aws_ami.default.id
   instance_type               = var.instance_type
   user_data                   = length(var.user_data_base64) > 0 ? var.user_data_base64 : data.template_file.user_data[0].rendered
